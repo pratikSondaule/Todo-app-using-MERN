f20ae86487c0f97d497fc20a07f79ccc1c489ba5
Pratik Sondaule <pratiksondaule@gmail.com>
2023-11-09 16:46:48 +0530

    changes done

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,15 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error(err);
+});
 
 app.use('/todo', todoRoutes);
 
